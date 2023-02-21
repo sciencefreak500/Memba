9f60c40086ca6b9f9cdd474e0a87ec4241583c28
Milord <immunitygame@gmail.com>
2023-02-21 00:15:40 -0500

    random value based on uuid

--- src/hooks/useTaskSorting.ts
+++ src/hooks/useTaskSorting.ts
@@ -22,6 +22,15 @@ type Task = {
     };
 };
 
+function getNormalizedValueFromUUID(uuid: string) {
+    const uuidArray = uuid.split('-').join('').split('');
+    const uuidArraySum = uuidArray.reduce((acc, curr) => acc + parseInt(curr, 16), 0);
+    // normalize the value to be between 0 and 1 
+    // (uuid is 32 chars long, max value is 15*32 = 480, min value is 0)
+    const normalizedValue = uuidArraySum / 480;
+    return normalizedValue;
+}
+
 
 function useTaskSorting() {
   const { data, loading, error } = useQuery(GET_TASKS_FOR_SORTING);
@@ -33,7 +42,7 @@ function useTaskSorting() {
 
   const getSortingValue = (task: Task) => {
     const m = task.state === 'BACKLOG' ? 0.3 : task.state === 'IN_PROGRESS' ? 0.6 : 0;
-    const c = Math.random();
+    const c = getNormalizedValueFromUUID(task.uuid);
     const b = task.Project.priority;
     // x = age of task, now - created_at in minutes
     const x = (new Date().getTime() - new Date(task.created_at).getTime()) / 1000 / 60;
